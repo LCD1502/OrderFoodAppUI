d87820d5789c32bddbf9f35b508f346af32b4a2c
hipngx <19520536@gm.uit.edu.vn>
2022-01-13 16:47:15 +0700

    Update thanh toan gio hang

--- OrderFoodAppUI/OrderFoodAppUI/Views/Bill.xaml.cs
+++ OrderFoodAppUI/OrderFoodAppUI/Views/Bill.xaml.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -27,7 +28,39 @@ namespace OrderFoodAppUI.Views
             BillUser = user;
             BillInit(carts, BillUser);
         }
-        void BillInit(List<Cart> cart, User user)
+
+        private static double DegreesToRadians(double degrees)
+        {
+            return degrees * Math.PI / 180.0;
+        }
+
+        public static double CalculateDistance(Location location1, Location location2)
+        {
+            double circumference = 40000.0; // Earth's circumference at the equator in km
+            double distance = 0.0;
+
+            double latitude1Rad = DegreesToRadians(location1.Latitude);
+            double longitude1Rad = DegreesToRadians(location1.Longitude);
+            double latititude2Rad = DegreesToRadians(location2.Latitude);
+            double longitude2Rad = DegreesToRadians(location2.Longitude);
+            double logitudeDiff = Math.Abs(longitude1Rad - longitude2Rad);
+
+            if (logitudeDiff > Math.PI)
+            {
+                logitudeDiff = 2.0 * Math.PI - logitudeDiff;
+            }
+
+            double angleCalculation =
+                Math.Acos(
+                  Math.Sin(latititude2Rad) * Math.Sin(latitude1Rad) +
+                  Math.Cos(latititude2Rad) * Math.Cos(latitude1Rad) * Math.Cos(logitudeDiff));
+
+            distance = circumference * angleCalculation / (2.0 * Math.PI);
+
+            distance = Math.Round(distance, 1);
+            return distance;
+        }
+        async void BillInit(List<Cart> cart, User user)
         {
             USname.Text = user.HOTEN;
             Time.Text = DateTime.Now.ToString();
@@ -39,6 +72,37 @@ namespace OrderFoodAppUI.Views
             Money.Text =tien.ToString();
             Ship.Text = "20000";
             Total.Text = (tien + 20000).ToString();
+
+            var location = await Geolocation.GetLastKnownLocationAsync();
+            var location2 = await Geolocation.GetLastKnownLocationAsync();
+
+            if (location == null)
+            {
+                location = await Geolocation.GetLocationAsync(new GeolocationRequest
+                {
+                    DesiredAccuracy = GeolocationAccuracy.Medium,
+                    Timeout = TimeSpan.FromSeconds(30)
+                });
+            }
+            if (location == null)
+                MyLocation.Text = "No GPS";
+            else
+                MyLocation.Text = $"{location.Latitude} {location.Longitude}";
+
+            var placemarks = await Geocoding.GetPlacemarksAsync(location.Latitude, location.Longitude);
+            var placemark = placemarks?.FirstOrDefault();
+
+            var Provine = placemark.AdminArea;
+            var District = placemark.SubAdminArea;
+            var SubAdd1 = placemark.FeatureName;
+            var SubAdd2 = placemark.Thoroughfare;
+                var tempaddress = SubAdd2 + ", " + SubAdd1 + ", " + District + ", " + Provine;       
+            MyLocation.Text = tempaddress;
+
+            //location2.Latitude = 13.05157847078659;
+            //location2.Longitude = 109.34687624119483;
+            //double distance = CalculateDistance(location, location2);
+            //USname.Text = distance.ToString();
         }
 
         private void BtnLater_Clicked(object sender, EventArgs e)
@@ -46,9 +110,31 @@ namespace OrderFoodAppUI.Views
             App.Current.MainPage.Navigation.PopAsync(true);
         }
 
-        private void BtnOrder_Clicked(object sender, EventArgs e)
+        private async void BtnOrder_Clicked(object sender, EventArgs e)
         {
+            DateTime time1 = DateTime.Now;
+            DateTime time2 = time1.AddMinutes(15);//15 phút sau
+            HttpClient httpClient = new HttpClient();
 
+            string tgdat = time1.ToString("MM/dd/yyyy HH:mm:ss"); // chuyen doi thoi gian hop le
+            tgdat=tgdat.Replace("SA", "AM");
+            tgdat = tgdat.Replace("CH", "PM");
+
+            string tggiao = time1.ToString("MM/dd/yyyy HH:mm:ss"); // chuyen doi thoi gian hop le 15 phut sau
+            tggiao = tggiao.Replace("SA", "AM");
+            tggiao = tggiao.Replace("CH", "PM");
+
+            var MAHD = await httpClient.GetStringAsync("http://appfood.somee.com/api/AppFoodController/InsertHoaDon?mand="+BillUser.MAND.ToString()+"&tongtien="+50000.ToString()+"&tgdat="+tgdat+"&tggiao="+tggiao+"&ship="+Ship.Text);
+            MAHD = MAHD.Replace("[{\"MAHD\":", string.Empty);
+            MAHD = MAHD.Replace(".0}]", string.Empty);
+            
+            foreach (Cart x in carts)
+            {
+                var aaa = await httpClient.GetStringAsync("http://appfood.somee.com/api/AppFoodController/InsertCTHoaDon?mahd="+MAHD+"&mama="+x.MAMA+"&soluong="+x.SOLUONG);
+            }
+
+            await DisplayAlert("Thông báo", "Đặt hàng thành công thời  gian giao hàng dự kiến: " + time2.ToString() + "\n" + MAHD, "ok");
+            App.Current.MainPage.Navigation.PopAsync(true);
         }
     }
 }
--- OrderFoodAppUI/OrderFoodAppUI/Views/HomeView.xaml.cs
+++ OrderFoodAppUI/OrderFoodAppUI/Views/HomeView.xaml.cs
@@ -17,13 +17,17 @@ namespace OrderFoodAppUI.Views
         {
             NavigationPage.SetHasNavigationBar(this, false);
             InitializeComponent();
+            //Placeinit();
         }
 
         public HomeView(User user)
         {
             NavigationPage.SetHasNavigationBar(this, false);
             InitializeComponent();
+            //await Placeinit();
             HomeUser = user;
+            Hello.Text = "Xin chào " + user.HOTEN;
+            
         }
 
         private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
@@ -36,7 +40,8 @@ namespace OrderFoodAppUI.Views
             App.Current.MainPage.Navigation.PushAsync(new CartView(), true);
         }
 
-        private async void GetLct_Clicked(object sender, EventArgs e)
+
+        private async void Placeinit()
         {
             try
             {
@@ -44,7 +49,7 @@ namespace OrderFoodAppUI.Views
                 if (location == null)
                 {
                     location = await Geolocation.GetLocationAsync(new GeolocationRequest
-                    { 
+                    {
                         DesiredAccuracy = GeolocationAccuracy.Medium,
                         Timeout = TimeSpan.FromSeconds(30)
                     });
@@ -52,29 +57,106 @@ namespace OrderFoodAppUI.Views
                 if (location == null)
                     LabelLocation.Text = "No GPS";
                 else
-                    LabelLocation.Text = $"{location.Latitude} {location.Longitude}";
-
-                var placemarks = await Geocoding.GetPlacemarksAsync(location.Latitude, location.Longitude);
-                var placemark = placemarks?.FirstOrDefault();
-
-                var geocodeAdress =
-               $"AdminArea:         {placemark.AdminArea}\n" +
-               $"CountryCode:       {placemark.CountryCode}\n" +
-               $"CountryName:       {placemark.CountryName}\n" +
-               $"FeatureName:       {placemark.FeatureName}\n" +
-               $"Locality:          {placemark.Locality}\n" +
-               $"PostalCode:        {placemark.PostalCode}\n" +
-               $"SubAdminArea:      {placemark.SubAdminArea}\n" +
-               $"SubLocality:       {placemark.SubLocality}\n" +
-               $"SubThoroughfare:   {placemark.SubThoroughfare}\n" +
-               $"Thoroughfare:      {placemark.Thoroughfare}\n";
-                LabelLocation2.Text = geocodeAdress;
+                {
+                    var placemarks = await Geocoding.GetPlacemarksAsync(location.Latitude, location.Longitude);
+                    var placemark = placemarks?.FirstOrDefault();
+                    var Provine = placemark.AdminArea;
+                    var District = placemark.SubAdminArea;
+                    var SubAdd1 = placemark.FeatureName;
+                    var SubAdd2 = placemark.Thoroughfare;
+                    var tempaddress = SubAdd2 + ", " + SubAdd1 + ", " + District + ", " + Provine;
+                    LabelLocation.Text = tempaddress;
+                }    
             }
             catch (Exception)
             {
                 await DisplayAlert("Error !!!", "Something's wrong", "Try again!");
             }
         }
+
+        private async void TapGestureRecognizer_Tapped_2(object sender, EventArgs e)
+        {
+            try
+            {
+                var location = await Geolocation.GetLastKnownLocationAsync();
+                if (location == null)
+                {
+                    location = await Geolocation.GetLocationAsync(new GeolocationRequest
+                    {
+                        DesiredAccuracy = GeolocationAccuracy.Medium,
+                        Timeout = TimeSpan.FromSeconds(30)
+                    });
+                }
+                if (location == null)
+                    LabelLocation.Text = "No GPS";
+                else
+                {
+                    var placemarks = await Geocoding.GetPlacemarksAsync(location.Latitude, location.Longitude);
+                    var placemark = placemarks?.FirstOrDefault();
+                    var Provine = placemark.AdminArea;
+                    var District = placemark.SubAdminArea;
+                    var SubAdd1 = placemark.FeatureName;
+                    var SubAdd2 = placemark.Thoroughfare;
+                    var tempaddress = SubAdd2 + ", " + SubAdd1 + ", " + District + ", " + Provine;
+                    LabelLocation.Text = tempaddress;
+                }
+            }
+            catch (Exception)
+            {
+                await DisplayAlert("Error !!!", "Something's wrong", "Try again!");
+            }
+        }
+        //private async void GetLct_Clicked(object sender, EventArgs e)
+        //{
+        //    try
+        //    {
+        //        var location = await Geolocation.GetLastKnownLocationAsync();
+        //        if (location == null)
+        //        {
+        //            location = await Geolocation.GetLocationAsync(new GeolocationRequest
+        //            { 
+        //                DesiredAccuracy = GeolocationAccuracy.Medium,
+        //                Timeout = TimeSpan.FromSeconds(30)
+        //            });
+        //        }
+        //        if (location == null)
+        //            LabelLocation.Text = "No GPS";
+        //        else
+        //            LabelLocation.Text = $"{location.Latitude} {location.Longitude}";
+
+        //        var placemarks = await Geocoding.GetPlacemarksAsync(location.Latitude, location.Longitude);
+        //        var placemark = placemarks?.FirstOrDefault();
+        //        var Provine = placemark.AdminArea;
+        //        var District = placemark.SubAdminArea;
+        //        var SubAdd1 = placemark.FeatureName;
+        //        var SubAdd2 = placemark.Thoroughfare;
+        //        string address;
+        //        //if (SubAdd2=="")
+
+        //        var tempaddress = SubAdd2 + ", " + SubAdd1 + ", " + District + ", " + Provine;
+
+        //        LabelLocation.Text = tempaddress;
+        //       // var geocodeAdress =
+        //       //$"AdminArea:         {placemark.AdminArea}\n" +
+        //       //$"CountryCode:       {placemark.CountryCode}\n" +
+        //       //$"CountryName:       {placemark.CountryName}\n" +
+        //       //$"FeatureName:       {placemark.FeatureName}\n" +
+        //       //$"Locality:          {placemark.Locality}\n" +
+        //       //$"PostalCode:        {placemark.PostalCode}\n" +
+        //       //$"SubAdminArea:      {placemark.SubAdminArea}\n" +
+        //       //$"SubLocality:       {placemark.SubLocality}\n" +
+        //       //$"SubThoroughfare:   {placemark.SubThoroughfare}\n" +
+        //       //$"Thoroughfare:      {placemark.Thoroughfare}\n";
+        //       // LabelLocation2.Text = geocodeAdress;
+        //    }
+        //    catch (Exception)
+        //    {
+        //        await DisplayAlert("Error !!!", "Something's wrong", "Try again!");
+        //    }
+        //}
+
+
+
 
         //public async void GetLocation()
         //{
